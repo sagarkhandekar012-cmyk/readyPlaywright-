131fa7d4394723ab0dcc9bfc8f28673c2ba14873
priyanshuvish <vishpriyanshu172@gmail.com>
2026-08-03 22:52:13 +0530

    new auto suggest dropdown selection updated

--- tests/autosuggestDropdown.spec.js
+++ tests/autosuggestDropdown.spec.js
@@ -1,13 +1,6 @@
 const {test , expect} = require('@playwright/test');
 
-test.use({ 
-    channel: 'chrome', 
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    extraHTTPHeaders: {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.9'
-    }
-});
+
 test('autosuggestDropdown', async ({page}) => {
 
     await page.goto('https://www.redbus.in/', { waitUntil: 'domcontentloaded' });
