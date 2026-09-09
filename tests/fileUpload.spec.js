895e1689821fcb3d1f2662c1d034a1e702db3b62
sagarkhandekar012-cmyk <sagarkhandekar012@gmail.com>
2026-09-10 00:11:34 +0530

    new updated

--- tests/fileUpload.spec.js
+++ tests/fileUpload.spec.js
@@ -3,27 +3,27 @@ const {test, expect} = require('@playwright/test');
 test('single upload a file', async ({ page }) => {
   await page.goto('https://companyadmin-amoz.betadelivery.com/login');
 
-await page.locator('//input[@placeholder="Enter Email Id"]').fill('rentavehicle24@yopmail.com');
-await page.locator('//input[@placeholder="Enter Password"]').fill('Test@123');
-await page.locator("//button[normalize-space()='Login']").click();
+  //email
+  const emailInput = page.locator('//input[@placeholder="Enter Email Id"]');
+  await emailInput.fill('rentavehicle24@yopmail.com');
+  await expect(emailInput).toHaveValue('rentavehicle24@yopmail.com');
+  
+
+  //password
+  const passwordInput = page.locator('//input[@placeholder="Enter Password"]');
+  await passwordInput.fill('Test@123');
+  await expect(passwordInput).toHaveValue('Test@123');
+  await page.locator("//button[normalize-space()='Login']").click();
 
 await page.locator("//span[normalize-space()='Vehicle Management']").click();
 await page.locator("//span[normalize-space()='Manage Vehicles']").click();
 await page.locator("//button[normalize-space()='Add New']").click();
 
-await page.waitForSelector("//label[@for='insuranceDocument']");
-await page.locator("//label[@for='insuranceDocument']").isEnabled();
-await page.locator("//input[@id='insuranceDocument']").setInputFiles("C:\\Users\\wdila\\Downloads\\2mb.pdf");
+const doc = page.locator("//label[@for='insuranceDocument']");
+//await doc.waitForSelector("//label[@for='insuranceDocument']");
+await doc.waitFor({ state: 'visible' });
+await doc.isEnabled();
+await doc.setInputFiles("C:\\Users\\wdila\\Downloads\\2mb.pdf");
 
   await page.waitForTimeout(5000);
 });
-
-/*
-test('multiple upload a file', async ({ page }) => {
-
-    await page.goto('');
-
-
-    await page.waitForTimeout(5000);
-});
-*/
