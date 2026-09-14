21d77d0465312f74da0dc97d0aaa5d7a73f86af3
Sagar Rajaram Khandekar <sagarkhandekar012@gmail.com>
2026-09-14 23:26:09 +0530

    Update Grouping test file with lifecycle hooks

--- tests/Grouping.spec.js
+++ tests/Grouping.spec.js
@@ -1,13 +1,55 @@
 import {test, expect} from '@playwright/test';
 
-/*
-test.beforeEach(async ({ page }) => {
+
+let page;
+let context;
+test.beforeAll(async ({ browser }) => {
+    context = await browser.newContext(); // Removed 'const'
+    page = await context.newPage();
+    console.log('.....statrt.....');
+});
+test.afterAll(async()=>{
+    console.log('-----END-----')
 })
-*/
 
-test('Grouping', async ({ page }) => {
+test.beforeEach(async () => {
+    console.log('login');
+
+        await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
+
+        await page.waitForTimeout(2000);
+
+    await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
+    await page.locator("//input[@placeholder='Enter your password']").fill('Admin@123');
+    await page.locator("//button[normalize-space()='Sign In']").click();
+
+await page.locator('//input[@aria-label="Character 1 of 4"]').fill('1');
+await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
+await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
+await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
+await page.locator("//button[normalize-space()='Verify']").click();
+})
+
+test.afterEach(async ()=>{
+
+    //logout 
+
+await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
+await page.locator("//span[normalize-space()='Sign Out']").click();
+await page.locator("//button[normalize-space()='Sign Out']").click();
+
+await page.waitForTimeout(2000);
+
+console.log('logout');
+})
+
+
+test.describe('group1',() =>{
+
+
+test('Test1', async () => {
   
-
+/*
     //this is login 
     await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
 
@@ -20,24 +62,23 @@ await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
 await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
 await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
 await page.locator("//button[normalize-space()='Verify']").click();
-
+*/
 
 await page.locator("//span[@class='rt-Text rt-r-size-6']").isVisible();
 
 
 //logout 
 
-await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-await page.locator("//span[normalize-space()='Sign Out']").click();
+//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
+//await page.locator("//span[normalize-space()='Sign Out']").click();
 
-    await page.waitForTimeout(5000);
 
 })
 
-test('Grouping2', async ({ page }) => {
+test('Test2', async () => {
 
     //login 
-
+/*
     await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
 
     await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
@@ -49,22 +90,28 @@ await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
 await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
 await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
 await page.locator("//button[normalize-space()='Verify']").click();
-
+*/
 
 const img = await page.locator("//img[@alt='A house in a forest']")
 await expect(img).toBeVisible();
 
 
 //logout 
-await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-await page.locator("//span[normalize-space()='Sign Out']").click();
+//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
+//await page.locator("//span[normalize-space()='Sign Out']").click();
 
 })
 
-test('Grouping3', async ({ page }) => {
+})// here is ending the Group1
+
+test.describe('group2',() =>{
+
+
+
+test('Test3', async () => {
 
     //login 
-    
+  /*  
 
     await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
 
@@ -77,22 +124,22 @@ await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
 await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
 await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
 await page.locator("//button[normalize-space()='Verify']").click();
-
+*/
 
 const text = await page.locator("//span[normalize-space()='Daily']")
 await expect(text).toHaveText("Daily"); 
 
 
 //logout 
-await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-await page.locator("//span[normalize-space()='Sign Out']").click();
+//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
+//await page.locator("//span[normalize-space()='Sign Out']").click();
 
 })
 
-test('Grouping4', async ({ page }) => {
+test('Test4', async () => {
 
     //login 
-
+/*
     await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
 
     await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
@@ -104,14 +151,36 @@ await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
 await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
 await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
 await page.locator("//button[normalize-space()='Verify']").click();
+*/
 
 
-const text = await page.locator("//span[normalize-space()='Daily']")
-await expect(text).toHaveText("Daily"); 
+// Fixed CSS selector with dots for multiple classes
+await page.locator("//button[normalize-space()='Manage Customers']").click();//rt-TableRootTable
+  const table = page.locator('.rt-TableRootTable');
+
+    // wait for the table to be ready before counting it
+   // await page.waitForTimeout(5000); // wait for 2 seconds to ensure the table is loaded
+    await table.waitFor();
+    await expect(table).toBeVisible();
+
+    // count columns as expected on this page
+    const columns = table.locator('thead tr th');
+    console.log('Number of columns: ' + await columns.count());
+   // expect(await columns.count()).toHaveCount(7);
+await expect(columns).toHaveCount(7);
+
+
+    // count rows as expected on this page
+    const rows = table.locator('tbody tr');
+    console.log('Number of rows: ' + await rows.count());
+    await expect(rows).toHaveCount(10);
+
 
 
 //logout 
-await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-await page.locator("//span[normalize-space()='Sign Out']").click();
+//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
+//await page.locator("//span[normalize-space()='Sign Out']").click();
 
 })
+
+})// here is ending the group2 
