875ae4b1d0a7c3ec77d8572a9fff733d0143a23a
Sagar Rajaram Khandekar <sagarkhandekar012@gmail.com>
2026-09-16 23:58:33 +0530

    coment have removed

--- tests/Grouping.spec.js
+++ tests/Grouping.spec.js
@@ -48,57 +48,15 @@ test.describe('group1',() =>{
 
 
 test('Test1', async () => {
-  
-/*
-    //this is login 
-    await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
-
-    await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
-    await page.locator("//input[@placeholder='Enter your password']").fill('Admin@123');
-    await page.locator("//button[normalize-space()='Sign In']").click();
-
-await page.locator('//input[@aria-label="Character 1 of 4"]').fill('1');
-await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
-await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
-await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
-await page.locator("//button[normalize-space()='Verify']").click();
-*/
 
 await page.locator("//span[@class='rt-Text rt-r-size-6']").isVisible();
-
-
-//logout 
-
-//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-//await page.locator("//span[normalize-space()='Sign Out']").click();
-
 
 })
 
 test('Test2', async () => {
 
-    //login 
-/*
-    await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
-
-    await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
-    await page.locator("//input[@placeholder='Enter your password']").fill('Admin@123');
-    await page.locator("//button[normalize-space()='Sign In']").click();
-
-await page.locator('//input[@aria-label="Character 1 of 4"]').fill('1');
-await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
-await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
-await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
-await page.locator("//button[normalize-space()='Verify']").click();
-*/
-
 const img = await page.locator("//img[@alt='A house in a forest']")
 await expect(img).toBeVisible();
-
-
-//logout 
-//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-//await page.locator("//span[normalize-space()='Sign Out']").click();
 
 })
 
@@ -110,49 +68,12 @@ test.describe('group2',() =>{
 
 test('Test3', async () => {
 
-    //login 
-  /*  
-
-    await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
-
-    await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
-    await page.locator("//input[@placeholder='Enter your password']").fill('Admin@123');
-    await page.locator("//button[normalize-space()='Sign In']").click();
-
-await page.locator('//input[@aria-label="Character 1 of 4"]').fill('1');
-await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
-await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
-await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
-await page.locator("//button[normalize-space()='Verify']").click();
-*/
-
 const text = await page.locator("//span[normalize-space()='Daily']")
 await expect(text).toHaveText("Daily"); 
-
-
-//logout 
-//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-//await page.locator("//span[normalize-space()='Sign Out']").click();
 
 })
 
 test('Test4', async () => {
-
-    //login 
-/*
-    await page.goto('https://testing.superadmin-amoz.betadelivery.com/login')
-
-    await page.locator("//input[@placeholder='Enter your email']").fill('amozit@yopmail.com');
-    await page.locator("//input[@placeholder='Enter your password']").fill('Admin@123');
-    await page.locator("//button[normalize-space()='Sign In']").click();
-
-await page.locator('//input[@aria-label="Character 1 of 4"]').fill('1');
-await page.locator('//input[@aria-label="Character 2 of 4"]').fill('2');
-await page.locator('//input[@aria-label="Character 3 of 4"]').fill('3');
-await page.locator('//input[@aria-label="Character 4 of 4"]').fill('4'); 
-await page.locator("//button[normalize-space()='Verify']").click();
-*/
-
 
 // Fixed CSS selector with dots for multiple classes
 await page.locator("//button[normalize-space()='Manage Customers']").click();//rt-TableRootTable
@@ -174,12 +95,6 @@ await expect(columns).toHaveCount(7);
     const rows = table.locator('tbody tr');
     console.log('Number of rows: ' + await rows.count());
     await expect(rows).toHaveCount(10);
-
-
-
-//logout 
-//await page.locator("//span[@class='rt-Text rt-r-size-2 rt-r-weight-bold']").click();
-//await page.locator("//span[normalize-space()='Sign Out']").click();
 
 })
 
