a0ad9b425cd507c9bb93516a17a2ebc9468bb5e3
Sagar Rajaram Khandekar <sagarkhandekar012@gmail.com>
2026-09-17 23:58:00 +0530

    new ss

---
+++ tests/ss.spec.js
@@ -0,0 +1,7 @@
+import { test } from '@playwright/test';
+
+test('Full page screenshot', async ({ page }) => {});
+
+test('Capture screenshot to buffer', async ({ page }) => {});
+
+test('Element screenshot', async ({ page }) => {});
